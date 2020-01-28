5ae91d3048f2ba2aa9dee8b7ccebb85271e49967
dioneuty <dioneuty@gmail.com>
2020-01-29 02:38:13 +0900

    게시판 페이징

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -5,28 +5,228 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    count : 1
-    ,menuIdx : 0
-    ,buletinTitle : '게시판'
-    ,bulThead : {title: '제목', cont: '내용'}
-    ,data : [
-      {title: '게시판1', content: '아 어려워1'},
-      {title: '게시판1', content: '아 어려워23'},
+    nCnt : 1
+    ,nMenuIdx : 0
+    ,sBuletinTitle : '게시판'
+    ,objBulThead : {title: '제목', cont: '내용'}
+    ,arrThread : [
+      [
+        {title: '게시판1', content: '아 어려워1'},
+        {title: '게시판1', content: '아 어려워23'},
+        {title: '게시판1', content: '아 어려워23'},
+        {title: '게시판1', content: '아 어려워23'},
+        {title: '게시판1', content: '아 어려워23'},
+        {title: '게시판1', content: '아 어려워23'},
+        {title: '게시판1', content: '아 어려워23'},
+        {title: '게시판1', content: '아 어려워23'},
+        {title: '게시판1', content: '아 어려워23'},
+        {title: '게시판1', content: '아 어려워23'},
+      ],
+      [
+        {title: '게시판2', content: '아 어려워1'},
+        {title: '게시판2', content: '아 어려워23'},
+        {title: '게시판2', content: '아 어려워23'},
+        {title: '게시판2', content: '아 어려워23'},
+        {title: '게시판2', content: '아 어려워23'},
+        {title: '게시판2', content: '아 어려워23'},
+        {title: '게시판2', content: '아 어려워23'},
+        {title: '게시판2', content: '아 어려워23'},
+        {title: '게시판2', content: '아 어려워23'},
+        {title: '게시판2', content: '아 어려워23'},
+      ],
+      [
+        {title: '게시판3', content: '아 어려워1'},
+        {title: '게시판3', content: '아 어려워23'},
+        {title: '게시판3', content: '아 어려워23'},
+        {title: '게시판3', content: '아 어려워23'},
+        {title: '게시판3', content: '아 어려워23'},
+        {title: '게시판3', content: '아 어려워23'},
+        {title: '게시판3', content: '아 어려워23'},
+        {title: '게시판3', content: '아 어려워23'},
+        {title: '게시판3', content: '아 어려워23'},
+        {title: '게시판3', content: '아 어려워23'},
+      ],
+      [
+        {title: '게시판4', content: '아 어려워1'},
+        {title: '게시판4', content: '아 어려워23'},
+        {title: '게시판4', content: '아 어려워23'},
+        {title: '게시판4', content: '아 어려워23'},
+        {title: '게시판4', content: '아 어려워23'},
+        {title: '게시판4', content: '아 어려워23'},
+        {title: '게시판4', content: '아 어려워23'},
+        {title: '게시판4', content: '아 어려워23'},
+        {title: '게시판4', content: '아 어려워23'},
+        {title: '게시판4', content: '아 어려워23'},
+      ],
+      [
+        {title: '게시판5', content: '아 어려워1'},
+        {title: '게시판5', content: '아 어려워23'},
+        {title: '게시판5', content: '아 어려워23'},
+        {title: '게시판5', content: '아 어려워23'},
+        {title: '게시판5', content: '아 어려워23'},
+        {title: '게시판5', content: '아 어려워23'},
+        {title: '게시판5', content: '아 어려워23'},
+        {title: '게시판5', content: '아 어려워23'},
+        {title: '게시판5', content: '아 어려워23'},
+        {title: '게시판5', content: '아 어려워23'},
+      ],
+      [
+        {title: '게시판6', content: '아 어려워1'},
+        {title: '게시판5', content: '아 어려워23'},
+        {title: '게시판5', content: '아 어려워23'},
+        {title: '게시판5', content: '아 어려워23'},
+        {title: '게시판5', content: '아 어려워23'},
+        {title: '게시판5', content: '아 어려워23'},
+        {title: '게시판5', content: '아 어려워23'},
+        {title: '게시판5', content: '아 어려워23'},
+        {title: '게시판5', content: '아 어려워23'},
+        {title: '게시판5', content: '아 어려워23'},
+      ],
+      [
+        {title: '게시판7', content: '아 어려워1'},
+        {title: '게시판5', content: '아 어려워23'},
+        {title: '게시판5', content: '아 어려워23'},
+        {title: '게시판5', content: '아 어려워23'},
+        {title: '게시판5', content: '아 어려워23'},
+        {title: '게시판5', content: '아 어려워23'},
+        {title: '게시판5', content: '아 어려워23'},
+        {title: '게시판5', content: '아 어려워23'},
+        {title: '게시판5', content: '아 어려워23'},
+        {title: '게시판5', content: '아 어려워23'},
+      ],
+      [
+        {title: '게시판8', content: '아 어려워1'},
+        {title: '게시판5', content: '아 어려워23'},
+        {title: '게시판5', content: '아 어려워23'},
+        {title: '게시판5', content: '아 어려워23'},
+        {title: '게시판5', content: '아 어려워23'},
+        {title: '게시판5', content: '아 어려워23'},
+        {title: '게시판5', content: '아 어려워23'},
+        {title: '게시판5', content: '아 어려워23'},
+        {title: '게시판5', content: '아 어려워23'},
+        {title: '게시판5', content: '아 어려워23'},
+      ],
+      [
+        {title: '게시판9', content: '아 어려워1'},
+        {title: '게시판5', content: '아 어려워23'},
+        {title: '게시판5', content: '아 어려워23'},
+        {title: '게시판5', content: '아 어려워23'},
+        {title: '게시판5', content: '아 어려워23'},
+        {title: '게시판5', content: '아 어려워23'},
+        {title: '게시판5', content: '아 어려워23'},
+        {title: '게시판5', content: '아 어려워23'},
+        {title: '게시판5', content: '아 어려워23'},
+        {title: '게시판5', content: '아 어려워23'},
+      ],
+      [
+        {title: '게시판10', content: '아 어려워1'},
+        {title: '게시판5', content: '아 어려워23'},
+        {title: '게시판5', content: '아 어려워23'},
+        {title: '게시판5', content: '아 어려워23'},
+        {title: '게시판5', content: '아 어려워23'},
+        {title: '게시판5', content: '아 어려워23'},
+        {title: '게시판5', content: '아 어려워23'},
+        {title: '게시판5', content: '아 어려워23'},
+        {title: '게시판5', content: '아 어려워23'},
+        {title: '게시판5', content: '아 어려워23'},
+      ],
+      [
+        {title: '게시판11', content: '아 어려워1'},
+        {title: '게시판5', content: '아 어려워23'},
+        {title: '게시판5', content: '아 어려워23'},
+        {title: '게시판5', content: '아 어려워23'},
+        {title: '게시판5', content: '아 어려워23'},
+        {title: '게시판5', content: '아 어려워23'},
+        {title: '게시판5', content: '아 어려워23'},
+        {title: '게시판5', content: '아 어려워23'},
+        {title: '게시판5', content: '아 어려워23'},
+        {title: '게시판5', content: '아 어려워23'},
+      ],
+      [
+        {title: '게시판12', content: '아 어려워1'},
+        {title: '게시판5', content: '아 어려워23'},
+        {title: '게시판5', content: '아 어려워23'},
+        {title: '게시판5', content: '아 어려워23'},
+        {title: '게시판5', content: '아 어려워23'},
+        {title: '게시판5', content: '아 어려워23'},
+        {title: '게시판5', content: '아 어려워23'},
+        {title: '게시판5', content: '아 어려워23'},
+        {title: '게시판5', content: '아 어려워23'},
+        {title: '게시판5', content: '아 어려워23'},
+      ],
+      [
+        {title: '게시판5', content: '아 어려워1'},
+        {title: '게시판5', content: '아 어려워23'},
+        {title: '게시판5', content: '아 어려워23'},
+        {title: '게시판5', content: '아 어려워23'},
+        {title: '게시판5', content: '아 어려워23'},
+        {title: '게시판5', content: '아 어려워23'},
+        {title: '게시판5', content: '아 어려워23'},
+        {title: '게시판5', content: '아 어려워23'},
+        {title: '게시판5', content: '아 어려워23'},
+        {title: '게시판5', content: '아 어려워23'},
+      ],
     ]
+    ,nScroll : 1
+    ,arrPage : []
   },
   mutations: {
-    increment (state) {
-      state.count++
+    init (state) {
+      let arr = [];
+      for(let i = 1; i <= state.arrThread.length; i++)
+      {
+        if(i > 10) break;
+
+        arr.push(i);
+      }
+
+      state.arrPage = arr;
+    }
+    ,increment (state) {
+      if(state.arrThread[state.nCnt] != undefined)
+      {
+        ++state.nCnt
+
+        if(state.nCnt > state.nScroll * 10){
+          ++state.nScroll;
+
+          let arr = [];
+          let nTmp = (state.nScroll - 1 ) * 10 + 1;
+          for(let i = nTmp; i < nTmp + 10; i++){
+            if(state.arrThread[i-1] != undefined) arr.push(i);
+          }
+
+          state.arrPage = arr;
+        }
+      }
     }
     ,decrement (state) {
-      if(state.count > 1) state.count--
+      if(state.arrThread[state.nCnt - 1] != undefined)
+      {
+        if(state.nCnt > 1) --state.nCnt;
+
+        if(state.nCnt <= (state.nScroll - 1) * 10){
+          if(state.nScroll > 1) --state.nScroll;
+
+          let arr = [];
+          let nTmp = (state.nScroll - 1 ) * 10 + 1;
+          for(let i = nTmp; i < nTmp + 10; i++){
+            if(state.arrThread[i-1] != undefined) arr.push(i);
+          }
+
+          state.arrPage = arr;
+        }   
+      }   
     }
-    ,incMenuIdx (state) {
-      state.menuIdx++
+    ,incnMenuIdx (state) {
+      state.nMenuIdx++
     }   
-    ,decMenuIdx (state) {
-      state.menuIdx--
+    ,decnMenuIdx (state) {
+      state.nMenuIdx--
     }       
+    ,movePageNum (state) {
+
+    }
   },
   actions: {
   },
--- frontend/src/views/Bulletin1.vue
+++ frontend/src/views/Bulletin1.vue
@@ -1,22 +1,48 @@
 <template>
   <div id="bulletin">
-    <h2>{{ $store.state.buletinTitle  }} </h2>
+    <h2>{{ $store.state.sBuletinTitle  }} </h2>
     <table class="table">
     <th>
       <td>
-      {{$store.state.bulThead.title}}
+      {{$store.state.objBulThead.title}}
       </td>
       <td>
-        {{$store.state.bulThead.cont}}
+        {{$store.state.objBulThead.cont}}
         </td>
     </th>
-    <tr v-for="(item, index)  in $store.state.data">
+    <tr v-for="(item, index)  in $store.state.arrThread[($store.state.nCnt - 1)]">
       <td>{{ item.title }}</td>
       <td>{{ item.content }}</td>
     </tr>  
     </table>  
+
+    <div>
+      <p>
+        <span v-for="(item, nIndex) in $store.state.arrPage">
+          <a v-if='item == $store.state.nCnt' style="color:red">{{item}} </a>
+          <a v-if='item != $store.state.nCnt' style="color:blue; cursor:pointer" v-on:click="$store.state.nCnt = (($store.state.nScroll -1) * 10) + nIndex + 1">{{item}} </a>
+        </span>
+      </p>
+    </div>
+    <div>
+    <button v-on:click="$store.commit('increment')"><<</button>    
     <button v-on:click="$store.commit('decrement')">prev</button>
-    <h1>{{$store.state.count}}</h1>
-    <button v-on:click="$store.commit('increment')">next</button>
+    <button v-on:click="$store.commit('increment')">next</button>    
+    <button v-on:click="$store.commit('increment')">>></button>    
+    </div>
+    <div>
+    <input type="text"/>
+    <button>검색</button>
+    </div>
   </div>
 </template>
+<script>
+import store from '@/store';
+
+export default {
+  name: 'bulletin',
+  created: function () {
+    store.commit('init');
+  }  
+}
+</script>
