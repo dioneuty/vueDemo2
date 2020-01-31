0b6d89d0b07e847e1e025fcfd5b8f68e1cc338ee
dioneuty <dioneuty@gmail.com>
2020-01-31 14:15:27 +0900

    prosdata

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld v-bind:propsdata = "msg"/>
   </div>
 </template>
 
@@ -13,6 +13,11 @@ export default {
   name: 'home',
   components: {
     HelloWorld
+  },
+  data() {
+    return {
+      msg : 'Welcome to Your Vue.js App'
+    }
   }
 }
 </script>
